3b1716723d1d3891962191f1a068c68670e8f7a4
EDUARDO GONÇALVES DE OLIVEIRA <23111718@dsn127m1242314.cfp127aluno.local>
2024-05-16 16:38:14 -0300

    languages done

--- app/src/main/java/com/example/mytrips/repositorio/ViagemRepositorio.kt
+++ app/src/main/java/com/example/mytrips/repositorio/ViagemRepositorio.kt
@@ -2,6 +2,7 @@ package com.example.mytrips.repositorio
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import com.example.mytrips.R
 import com.example.mytrips.modelo.Viagem
 import java.time.LocalDate
@@ -14,7 +15,7 @@ class ViagemRepositorio {
         val londres = Viagem(
             1,
             "Londres",
-            "Londres, capital da Inglaterra e do Reino Unido, é uma cidade do século 21 com uma história que remonta à era romana. ",
+            stringResource(id = R.string.london),
             LocalDate.of(2019, 2, 18),
             LocalDate.of(2019, 2, 21),
             imagem = painterResource(id = R.drawable.london)
@@ -23,7 +24,7 @@ class ViagemRepositorio {
         val porto = Viagem(
             1,
             "Porto",
-            "Porto é uma cidade costeira no noroeste de Portugal conhecida pelas pontes imponentes e pela produção de vinho do Porto.",
+            stringResource(id = R.string.porto),
             LocalDate.of(2022, 2, 21),
             LocalDate.of(2025, 1, 1),
             imagem = painterResource(id = R.drawable.porto)
@@ -32,7 +33,7 @@ class ViagemRepositorio {
         val ontario = Viagem(
             2,
             "Ontário",
-            "Ontário é uma província do Canada que faz fronteira com os Estados Unidos e os Grandes Lagos",
+            stringResource(id = R.string.ontario),
             LocalDate.of(2020, 7, 28),
             LocalDate.of(2025, 7, 22),
             imagem = painterResource(id = R.drawable.canada)
@@ -41,7 +42,7 @@ class ViagemRepositorio {
         val franca = Viagem(
             3,
             "Paris",
-            "A França, na Europa Ocidental, tem cidades medievais, aldeias alpinas e praias mediterrâneas.",
+            stringResource(id = R.string.paris),
             LocalDate.of(2025, 12, 23),
             LocalDate.of(2026, 2, 28),
             imagem = painterResource(id = R.drawable.placeholderimage)
@@ -50,7 +51,7 @@ class ViagemRepositorio {
         val osasco = Viagem(
             4,
             "Osasco",
-            "Osasco é um município brasileiro localizado na Região Metropolitana de São Paulo, no estado de São Paulo, no Brasil.",
+            stringResource(id = R.string.osasco),
             LocalDate.of(2024, 4, 19),
             LocalDate.of(2024, 4, 19),
             imagem = painterResource(id = R.drawable.osasco)
@@ -59,7 +60,7 @@ class ViagemRepositorio {
         val carapicuiba = Viagem(
             5,
             "Carapicuíba",
-            "Carapicuíba é um município no estado de São Paulo, Brasil, e faz parte da Região Metropolitana de São Paulo.",
+            stringResource(id = R.string.carapicuiba),
             LocalDate.of(2024, 4, 19),
             LocalDate.of(2024, 4, 23),
             imagem = painterResource(id = R.drawable.carapicuiba)
